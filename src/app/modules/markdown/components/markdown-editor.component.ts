5a95acda9e05f0d2d90ae59bc5071a7823a85164
metasong <metaseed@gmail.com>
2017-10-23 14:44:58 +0800

    ok

--- src/app/modules/markdown/components/markdown-editor.component.ts
+++ src/app/modules/markdown/components/markdown-editor.component.ts
@@ -1,12 +1,4 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-
-/**
- * MarkdownEditorComponent
- *
- * @export
- * @class MarkdownEditorComponent
- */
 @Component({
   selector: 'markdown-editor',
   templateUrl: 'markdown-editor.component.html'
--- src/app/modules/markdown/components/markdown-viewer.component.ts
+++ src/app/modules/markdown/components/markdown-viewer.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MarkdownViewService } from '../services/markdown.view.service';
-
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'markdown-viewer',
-  templateUrl: 'markdown-viewer.component.html'
+  templateUrl: 'markdown-viewer.component.html',
+  styleUrls: [
+  ]
 })
 export class MarkdownViewerComponent {
   @Input()
