09c4bf165d8352263c30e1b884eab57a4287cf7c
metasong <metaseed@gmail.com>
2017-11-08 16:50:49 +0800

    ok

--- workspaces/iam/src/app/docs/models/document.ts
+++ workspaces/iam/src/app/docs/models/document.ts
@@ -1,10 +1,6 @@
-export interface Document {
-    id: number;
-    title: string;
-    labels: string; // comma separated
-    creator: string;
-    state: 'open' | 'closed' | 'all';
-    created_at: Date;
-    updated_at: Date;
-    comments: object;
+import { Issue } from "../../storage/github/issues/issue";
+
+
+export interface Document extends Issue {
+
 }
