9fc6877427e308d664f131dfa434bf9e3a4ee5d2
metasong <metaseed@gmail.com>
2017-12-12 17:50:43 +0800

    ok

--- workspaces/iam/src/main.ts
+++ workspaces/iam/src/main.ts
@@ -14,7 +14,7 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('ngsw-worker.js');
+      navigator.serviceWorker.register('/ngsw-worker.js');
     }
   })
   .catch(err => console.log(err));
