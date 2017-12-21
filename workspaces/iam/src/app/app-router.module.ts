e55db779be7991005e21b4e45a1904daf96cb1d2
metasong <metaseed@gmail.com>
2017-12-21 11:24:42 +0800

    ok

--- workspaces/iam/src/app/app-router.module.ts
+++ workspaces/iam/src/app/app-router.module.ts
@@ -1,14 +1,24 @@
 import { Routes } from "@angular/router";
 import { NotFoundComponent } from './not-found/not-found.component';
-import { DocListComponent } from "./docs/doc-list/doc-list.component";
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router/src/router_module";
 import { SelectivePreloadingStrategy } from "./selective-preloading-strategy";
 import { CanDeactivateGuard } from "./can-deactivate-guard.service";
+import { DocsComponent } from "./docs/docs.component";
 
 const appRoutes: Routes = [
+    // {
+    //     path: 'admin',
+    //     loadChildren: 'app/admin/admin.module#AdminModule',
+    //     canLoad: [AuthGuard]
+    //   },
+    //   {
+    //     path: 'crisis-center',
+    //     loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule',
+    //     data: { preload: true }
+    //   },
     { path: 'doc', component:  },
-    { path: '', component: DocListComponent },
+    { path: '', component: DocsComponent },
     { path: '**', component: NotFoundComponent }
 ];
 
