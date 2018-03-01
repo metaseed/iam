8308f96be4a12ab720713c2a783b16f1e650fe2f
metasong <metaseed@gmail.com>
2018-03-01 18:05:44 +0800

    z-index of code mirror

--- workspaces/iam/src/app/modules/markdown/reducers/edit.ts
+++ workspaces/iam/src/app/modules/markdown/reducers/edit.ts
@@ -29,7 +29,7 @@ export function reducer(state = initialState, action: EditActions) {
         case EditActionTypes.LockScrollWithView: {
             return {
                 ...state,
-                lockScrollWithView: action.payload;
+                lockScrollWithView: action.payload
             }
         }
         default:
