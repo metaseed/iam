537e5f9362b8e51c7d90969ba8b53895b00bd8aa
metasong <metaseed@gmail.com>
2018-04-16 09:25:33 +0800

    :sparkles: hmr

--- workspaces/iam/src/app/app.component.ts
+++ workspaces/iam/src/app/app.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Repository, } from './storage/github/index';
-import { GithubStorage } from './storage/github/github';
-import { UserInfo } from './storage/github/user-info';
-import { UpdateService } from 'core';
-import { environment } from '../environments/environment';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Repository } from "./storage/github/index";
+import { GithubStorage } from "./storage/github/github";
+import { UserInfo } from "./storage/github/user-info";
+import { UpdateService } from "core";
+import { environment } from "../environments/environment";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   title = "I'm";
-  constructor(private _http: HttpClient, private _updateService: UpdateService) {
+  constructor(
+    private _http: HttpClient,
+    private _updateService: UpdateService
+  ) {
     if (environment.production) {
       this._updateService.checkForUpdate();
     }
   }
-  ngOnInit() {
-
-  }
-  private markdown = '';
+  ngOnInit() {}
+  private markdown = "";
 }
--- workspaces/iam/src/main.ts
+++ workspaces/iam/src/main.ts
@@ -1,20 +1,30 @@
 //import './polyfills';
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 // import 'hammerjs';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
+import { hmrBootstrap } from "./hmr";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(() => {
-    // if ('serviceWorker' in navigator) {
-    //   navigator.serviceWorker.register('/ngsw-worker.js');
-    // }
-  })
-  .catch(err => console.log(err));
+const bootstrap = platformBrowserDynamic().bootstrapModule(AppModule);
+// .then(() => {
+//   // if ('serviceWorker' in navigator) {
+//   //   navigator.serviceWorker.register('/ngsw-worker.js');
+//   // }
+// })
+// .catch(err => console.log(err));
+
+if (environment.hmr) {
+  if (module["hot"]) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error("HMR is not enabled for webpack-dev-server!");
+    console.log("Are you using the --hmr flag for ng serve?");
+  }
+}
