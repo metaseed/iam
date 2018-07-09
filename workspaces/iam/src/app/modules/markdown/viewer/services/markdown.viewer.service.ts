b902a59b1ec2524eb5d633f43b12365da823d675
metasong <metaseed@gmail.com>
2018-07-09 18:10:45 +0800

    add(copy button, wrap button)

--- workspaces/iam/src/app/modules/markdown/viewer/services/markdown.viewer.service.ts
+++ workspaces/iam/src/app/modules/markdown/viewer/services/markdown.viewer.service.ts
@@ -112,7 +112,7 @@ export class MarkdownViewerService {
           return `${getAddr(this.document.document.location.href)}#${slug}`;
         },
         permalinkClass: 'deep-link',
-        permalinkSymbol: `<svg aria-hidden="true" class="deep-link-icon" height="16" version="1.1" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`, //"¶",
+        permalinkSymbol: `<i class="material-icons deep-link-icon">link</i>`, //"¶",
         permalinkBefore: false
       })
       .use(toc, {
@@ -155,7 +155,7 @@ export class MarkdownViewerService {
       try {
         prismjs.highlightElement(codeNode);
 
-        return `<div class="markdown-code" style="position:relative"><button class="material-icons copy-button no-print"
+        return `<div class="markdown-code" style="position:relative"><button class="material-icons wrap-button" onclick="const e=event.target.parentElement.getElementsByTagName('code')[0];if(e.wrap){e.style['white-space']='pre';e.wrap=false}else{e.style['white-space']='pre-wrap';e.wrap=true}">wrap_text</button><button class="material-icons copy-button no-print"
         title="Copy code snippet"
         originalstr=${base64Encode(str)}
         onclick="document.copier.copyText(this.attributes.originalstr.value,true)">
