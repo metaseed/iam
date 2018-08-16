4eb432052cf6c556b89581503405a62fb41420d7
metasong <metaseed@gmail.com>
2018-08-16 11:21:08 +0800

    improvement lisp

--- workspaces/iam/src/app/modules/markdown/viewer/services/markdown-it-plugins/lisp.ts
+++ workspaces/iam/src/app/modules/markdown/viewer/services/markdown-it-plugins/lisp.ts
@@ -2,7 +2,7 @@ import * as MarkdownIt from 'markdown-it';
 import { littleLisp } from 'lispjs';
 export class LispPlugin {
   config = {
-    markerPattern: /^\.\([\s\S]*?\)\./m
+    markerPattern: /\*\([\s\S]*?\)\*/m
   };
 
   gState;
@@ -22,18 +22,29 @@ export class LispPlugin {
     };
 
     md.renderer.rules.lisp_body = (tokens, index) => {
-      const r = littleLisp.execute(tokens[index].content); // this.gState.tokens
-      return r;
+      try {
+        const r = littleLisp.execute(tokens[index].content); // this.gState.tokens
+        return r;
+      } catch (e) {
+        console.error(e);
+        return e;
+      }
     };
 
     md.core.ruler.push('grab_state', function(state) {
       this.gState = state;
     });
 
-    md.inline.ruler.before('text', 'lisp', this.lisp);
+    md.inline.ruler.before('backticks', 'lisp', this.parse);
   };
 
-  lisp = (state, silent?: boolean) => {
+  parse = (state, silent?: boolean) => {
+    const pos = state.pos;
+    const max = state.posMax;
+
+    let token;
+    let match;
+    let start: number;
     // Don't run any pairs in validation mode
     if (silent) {
       return false;
@@ -41,23 +52,29 @@ export class LispPlugin {
 
     // Reject if the token does not start with .
     if (
-      state.src.charCodeAt(state.pos) !== 0x2e /* . */ ||
-      state.src.charCodeAt(state.pos + 1) !== 0x28 /* ( */
+      state.src.charCodeAt(pos) !== 0x2a /* * */ ||
+      state.src.charCodeAt(pos + 1) !== 0x28 /* ( */
     ) {
       return false;
     }
 
-    let token;
-    let match;
-    const max = state.posMax;
+    start = pos;
+
+    // while(pos < max && )
+
     // Detect lisp markdown
     match = this.config.markerPattern.exec(state.src);
     match = !match
       ? []
-      : match.filter(function(m) {
+      : match.filter(function(m: string) {
           // validate here!
-
-          return true;
+          let counter = 0;
+          for (let i = 0; i < m.length; i++) {
+            const charCode = m.charCodeAt(i);
+            if (charCode === 0x28 /* ( */) counter += 1;
+            if (charCode === 0x29 /* ) */) counter -= 1;
+          }
+          return counter === 0;
         });
     if (match.length < 1) {
       return false;
@@ -70,8 +87,8 @@ export class LispPlugin {
       const lispExp = match[0].slice(1, -1);
       // Build content
       token = state.push('lisp_open', 'lisp', 1);
-      token.markup = '.(lisp).';
-      token = state.push('lisp_body', '', 0);
+      token.markup = '*()*';
+      token = state.push('lisp_body', 'lisp-body', 0);
       token.content = lispExp;
       token = state.push('lisp_close', 'lisp', -1);
       return true;
