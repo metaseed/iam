80622791a658b6c0f0ca340f72904c091b598b48
SONG Jianzhong <jsong12@slb.com>
2018-09-29 13:25:39 +0800

    Auto-generated commit

--- assets/js/global-script.js
+++ assets/js/global-script.js
@@ -1,3 +1,12 @@
+window.addEventListener(
+  'touchstart',
+  function onFirstTouch() {
+    window.USER_IS_TOUCHING = true;
+    window.removeEventListener('touchstart', onFirstTouch, false);
+  },
+  false
+);
+
 function wrap_text(event) {
   const e = event.target.parentElement.parentElement.getElementsByTagName('code')[0];
   if (!e.nowrap) {
@@ -9,7 +18,20 @@ function wrap_text(event) {
   }
 }
 
-function edit_event(target) {
+function md_edit_mouseenter() {
+  if (window.USER_IS_TOUCHING) event.target.setAttribute('data-mouseenter', Date.now());
+}
+function md_edit_mouseleave() {
+  if (window.USER_IS_TOUCHING) event.target.removeAttribute('data-mouseenter');
+}
+function md_edit_event(target) {
+  if (window.USER_IS_TOUCHING) {
+    const dateTick = target.getAttribute('data-mouseenter');
+    const diff = Date.now() - dateTick;
+    // fix for mobile touch(hover is triggered by and before click),
+    // because the hover(mouseenter) and click is triggered simutinously
+    if (diff < 200) return;
+  }
   const element = target;
   element.dispatchEvent(
     new CustomEvent('edit-it', {
