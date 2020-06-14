fb5295b2309dd94aad803b5319abe95dc9b33c62
metaseed <metaseed@gmail.com>
2020-06-13 22:26:39 -0500

    ok

--- workspaces/iam/src/app/modules/net-storage/github/github-storage.module.ts
+++ workspaces/iam/src/app/modules/net-storage/github/github-storage.module.ts
@@ -13,10 +13,11 @@ import { AuthInterceptor } from './auth-interceptor';
     GithubStorage,
     {
       provide: GITHUB_AUTHENTICATION,
-      useValue: new UserInfo('metasong', 'metaseed@gmail.com','c1ca0e600bbd517703a55facce5f788a0b470e52')
+      useValue: new UserInfo('metasong', 'metaseed@gmail.com',undefined,
+      [10, 80, 8, 12, 4, 84, 94, 80, 11, 90, 86, 91, 89, 5, 91, 80, 2, 89, 10, 2, 11, 13, 80, 15, 90, 85, 85, 92, 4, 9, 12, 81, 88, 12, 86, 8, 12, 85, 84, 8])
     },
     AuthService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 })
-export class GithubStorageModule {}
+export class GithubStorageModule { }
--- workspaces/iam/src/app/modules/net-storage/github/user-info.ts
+++ workspaces/iam/src/app/modules/net-storage/github/user-info.ts
@@ -3,6 +3,22 @@ export class UserInfo {
     constructor(public name: string,
         public email: string,
         public password?: string,
-        public token?: string
-    ) { }
+        token?: number[]
+    ) { 
+        this.token = UserInfo.dec(token, 'iam')
+    }
+    token:string;
+    static en(text: string, key:string) {
+        return Array.from(
+            text,
+            (c, i) => c.charCodeAt(0) ^ key.charCodeAt(i % key.length)
+        );
+    }
+
+    static dec(text:number[], key:string) {
+        return Array.from(
+            text,
+            (c, i) => String.fromCharCode(c ^ key.charCodeAt(i % key.length))
+        ).join('');
+    }
 }
