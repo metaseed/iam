dafaa1e23e227b001b6f3940c962460014cba502
metaseed <metaseed@gmail.com>
2021-10-07 10:29:39 -0500

    ok

--- workspaces/iam/src/app/modules/markdown/viewer/elements/tags/tags.component.ts
+++ workspaces/iam/src/app/modules/markdown/viewer/elements/tags/tags.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from "@angular/core";
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {MatChipInputEvent} from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 import { DataSourceLines } from "../data-source-lines";
 import { State, Store } from "@ngrx/store";
 import { Router } from "@angular/router";
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { debounceTime, Subject, tap } from "rxjs";
 @Component({
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.scss'],
@@ -16,12 +17,19 @@ export class TagsComponent extends DataSourceLines {
   addOnBlur = true;
   tagList: string[];
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  private modifyTags = new Subject();
 
   @Input() set tags(value: string) {
-    this.tagList = value.split(',');
+    this.tagList = value.split(',').map(tag=>tag.trim());
   }
   constructor(private router: Router, private store: Store<any>, private state: State<any>) {
     super(store, state);
+    this.modifyTags.pipe(
+      debounceTime(2000),
+      // tap(tags => {
+      //   this.source = `tag: [${tags}]`;
+      // })
+    ).subscribe();
   }
 
   add(event: MatChipInputEvent): void {
@@ -29,9 +37,9 @@ export class TagsComponent extends DataSourceLines {
 
     if (value) {
       this.tagList.push(value);
+      this.modifyTags.next(this.tagList);
     }
 
-    // Clear the input value
     event.chipInput!.clear();
   }
 
@@ -40,9 +48,12 @@ export class TagsComponent extends DataSourceLines {
 
     if (index >= 0) {
       this.tagList.splice(index, 1);
+      this.modifyTags.next(this.tagList);
     }
   }
+
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.tagList, event.previousIndex, event.currentIndex);
+    this.modifyTags.next(this.tagList);
   }
 }
