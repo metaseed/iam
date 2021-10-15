767bbc678233ccb2ec395566ab82c999ba663d9a
metaseed <metaseed@gmail.com>
2021-10-14 23:18:42 -0500

    ok

--- workspaces/iam/src/app/modules/store/spec/store.spec.ts
+++ workspaces/iam/src/app/modules/store/spec/store.spec.ts
@@ -1,15 +1,41 @@
 
-import {} from 'jasmine';
+import { } from 'jasmine';
+import { NEVER } from 'rxjs';
+import { map, of } from 'rxjs';
+import { state, StateObservable, StateSubject } from '../src';
 
-describe('simple-ts-file', () => {
-  it('should work', () => {
-    expect(true).toBeTrue()
-  })
-})
+describe('StateObservable', () => {
+  it('should add last value after state it in the last pipe', () => {
+    const o$ = of(1, 2).pipe(map(o => ++o), state) as unknown as StateObservable<number>;
+    o$.subscribe();
 
+    expect(o$.value).toBe(3);
+  });
 
-describe('simple-ts-file', () => {
-  it('should work', () => {
-    expect(true).toBeTrue()
+  it('should add last value after apply on Observable', () => {
+    const o$ = state(of(1, 2).pipe(map(o => ++o)));
+    o$.subscribe();
+
+    expect(o$.value).toBe(3);
+  });
+
+  it('should be undefined if no value emitted', ()=> {
+    const o$ = state(NEVER.pipe(map(o=>++o)));
+    o$.subscribe();
+    expect(o$.value).toBe(undefined)
+  });
+});
+
+describe('StateSubject', () => {
+  it('value should be undefined if not send any value, should keep the last send value', () => {
+    const s_ = new StateSubject<number>();
+    const o$ = s_.pipe(map(o=>++o), state) as unknown as StateObservable<number>;
+    o$.subscribe();
+    expect(s_.value).toBe(undefined);
+
+    s_.next(1);
+    s_.next(2);
+    expect(o$.value).toBe(3);
+    expect(s_.value).toBe(2);
   })
 })
--- workspaces/iam/src/app/modules/store/src/store.ts
+++ workspaces/iam/src/app/modules/store/src/store.ts
@@ -1,8 +1,14 @@
-import { Observable, tap } from "rxjs";
+import { Observable, tap, BehaviorSubject } from "rxjs";
 
 export type StateObservable<T> = Observable<T> & { value: T }
 
-export function state<T>(getState: () => Observable<T>): StateObservable<T> {
-  let state;
-  return tap<T>(o => (state ?? getState() as any).value = o)  as unknown as StateObservable<T>;
+export class StateSubject<T> extends BehaviorSubject<T> {
+  constructor(value?: T) {
+    super(value as T);
+  }
+}
+
+export function state<T>(source: Observable<T>): StateObservable<T> {
+  let state = tap<T>(o => (state as any).value = o)(source) as unknown as StateObservable<T>;
+  return state;
 }
