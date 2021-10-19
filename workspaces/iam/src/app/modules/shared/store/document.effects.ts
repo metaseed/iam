42da2e9c5d7bcb43e2f190ddab74cc8f1715b507
metaseed <metaseed@gmail.com>
2021-10-19 17:08:56 -0500

    ok

--- workspaces/iam/src/app/modules/shared/store/document.effects.ts
+++ workspaces/iam/src/app/modules/shared/store/document.effects.ts
@@ -1,4 +1,13 @@
+import { Injectable, InjectionToken } from "@angular/core";
+import { StateSubject } from "packages/rx-store/src";
 
+export interface IDocumentEffects{
+  CreateDocument: StateSubject<;
+}
+
+export const DOCUMENT_EFFECTS_TOKEN = new InjectionToken<IDocumentEffects>('DOCUMENT_EFFECTS_TOKEN');
+
+@Injectable({providedIn:'root'})
 export class DocumentsEffects {
 
 }
