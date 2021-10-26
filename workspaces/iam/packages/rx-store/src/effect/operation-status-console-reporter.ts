6d45c9366fdb9ac7a04357ead5945b490af6c67a
metaseed <metaseed@gmail.com>
2021-10-26 11:11:07 -0500

    status console report

--- workspaces/iam/packages/rx-store/src/effect/operation-status-console-reporter.ts
+++ workspaces/iam/packages/rx-store/src/effect/operation-status-console-reporter.ts
@@ -3,9 +3,26 @@ import { OperationStatusReporter } from "./operation-status-reporter.interface";
 import { OperationState } from "./operation-state";
 
 export class OperationStatusConsoleReporter implements OperationStatusReporter {
-   setup(operationStatusStore: OperationState) {
+  setup(operationStatusStore: OperationState) {
+
     operationStatusStore.start$.pipe(tap((status) => {
       console.log(`%c${status.id}->start`, 'background-color:#4285f4');
+    })).subscribe();
+
+    operationStatusStore.continue$.pipe(tap((status) => {
+      console.groupCollapsed(`%c${status.id}->continue`, 'background-color:#4285f4');
+      console.count(`${status.id}->continue`);
+      console.log('result:', status.context);
+      console.groupEnd();
+    })).subscribe();
+
+    operationStatusStore.retry$.pipe(tap((status) => {
+      console.log(`%c${status.id}->retry`, 'background-color:#4285f4');
+    })).subscribe();
+
+    operationStatusStore.fail$.pipe(tap((status) => {
+      console.log(`%c${status.id}->fail`, 'background-color:#F00');
+      console.error(status.context); // assume it's an Error object
     })).subscribe();
 
     operationStatusStore.complete$.pipe(tap((status) => {
@@ -13,18 +30,6 @@ export class OperationStatusConsoleReporter implements OperationStatusReporter {
       const msg = `${id}->complete`;
       console.groupCollapsed(`%c${msg}`, 'background-color:#4285f4');
       console.count(msg);
-      console.groupEnd();
-    })).subscribe();
-
-    operationStatusStore.error$.pipe(tap((status) => {
-      console.log(`%c${status.id}->error`, 'background-color:#F00');
-      console.error(status.context); // assume it's an Error object
-    })).subscribe();
-
-    operationStatusStore.continue$.pipe(tap((status) => {
-      console.groupCollapsed(`%c${status.id}->succession`, 'background-color:#4285f4');
-      console.count(`${status.id}->succession`);
-      console.log('result:', status.context);
       console.groupEnd();
     })).subscribe();
   }
