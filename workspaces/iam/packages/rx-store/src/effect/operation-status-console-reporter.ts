7ee7a8e78099516bbcb7637c8dd388c3a163ae58
metaseed <metaseed@gmail.com>
2021-11-04 22:16:23 -0500

    ok

--- workspaces/iam/packages/rx-store/src/effect/operation-status-console-reporter.ts
+++ workspaces/iam/packages/rx-store/src/effect/operation-status-console-reporter.ts
@@ -5,17 +5,20 @@ import { OperationStep } from "./operation-status";
 
 export class OperationStatusConsoleReporter implements OperationStatusReporter {
   setup(operationStatusStore: OperationState) {
-    const icons = ['💖','🧾','🔖','📘','🎉','😉','📚','👻','👀','✌','✨','🎨','⚽','🎁','🎗','🎞','🧧','🎇','🥼','🛒','👙','👜','🎭','🎀','🎄','💎','🧩','☎']
+    const icons =
+      ['💖', '🧾', '🔖', '📘', '🎉', '😉', '📚', '👻', '👀', '✌',
+        '✨', '🎨', '⚽', '🎁', '🎗', '🎞', '🧧', '🎇', '🥼', '🛒',
+        '👙', '👜', '🎭', '🎀', '🎄', '💎', '🧩', '☎', '🎃','🏈']
 
     operationStatusStore.ofStep(OperationStep.Start).pipe(tap((status) => {
-      const icon = icons[status.coId!%icons.length]
+      const icon = icons[status.coId! % icons.length]
       console.groupCollapsed(`%c${status.id}->start${icon}`, 'background-color:#abdcfb; color: black');
       console.log(status)
       console.groupEnd();
     })).subscribe();
 
     operationStatusStore.ofStep(OperationStep.Continue).pipe(tap((status) => {
-      const icon = icons[status.coId!%icons.length]
+      const icon = icons[status.coId! % icons.length]
       console.groupCollapsed(`%c${status.id}${icon}->continue`, 'background-color:purple; color: white');
       console.count(`${status.id}->continue`);
       console.log('result:', status.context);
@@ -42,7 +45,7 @@ export class OperationStatusConsoleReporter implements OperationStatusReporter {
     })).subscribe();
 
     operationStatusStore.ofStep(OperationStep.Success).pipe(tap((status) => {
-      const icon = icons[status.coId!%icons.length]
+      const icon = icons[status.coId! % icons.length]
       const id = status.id;
       const msg = `${id}->Success${icon}`;
       console.groupCollapsed(`%c${msg}`, 'background-color:#bada55; color: black');
