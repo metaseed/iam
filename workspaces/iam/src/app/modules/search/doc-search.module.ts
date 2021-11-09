50dc405efa52f7c63eb6777738bf3bbfc51530ea
metaseed <metaseed@gmail.com>
2021-11-09 16:28:25 -0600

    fix

--- workspaces/iam/src/app/modules/search/doc-search.module.ts
+++ workspaces/iam/src/app/modules/search/doc-search.module.ts
@@ -3,11 +3,12 @@ import { SharedModule } from 'shared';
 import { StoreSearchService } from '../cache/services/store-search.service';
 import { SearchRoutingModule } from './doc-search-routing.module';
 import { MaterialModule } from 'material';
+import { SearchPageComponent } from './doc-search.component';
 
 @NgModule({
   imports: [SharedModule, MaterialModule, SearchRoutingModule],
   exports: [],
-  declarations: [],
+  declarations: [SearchPageComponent],
   providers: [StoreSearchService]
 })
 export class DocSearchModule {}
