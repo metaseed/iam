5813cfe2da71619174f81ccf43acbb9b033de9b4
metaseed <metaseed@gmail.com>
2021-11-12 21:00:36 -0600

    update

--- workspaces/iam/src/app/modules/core/services/update.service.ts
+++ workspaces/iam/src/app/modules/core/services/update.service.ts
@@ -1,35 +1,42 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
-import { interval, Observable } from 'rxjs';
+import { ApplicationRef, Injectable } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, first, interval, map } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MSG_DISPLAY_TIMEOUT } from 'core';
+import { concat } from 'rxjs';
 
 @Injectable()
 export class UpdateService {
-  constructor(private swUpdate: SwUpdate, public snackBar: MatSnackBar) {
+  constructor(appRef: ApplicationRef, private swUpdate: SwUpdate, public snackBar: MatSnackBar) {
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
     // check update every hour
-    interval(1000 * 60 * 60).subscribe(() => this.checkForUpdate());
+    const everyOneHour$ = interval(1000 * 60 * 60);
+    concat(appIsStable$, everyOneHour$).subscribe(() => this.checkForUpdate());
 
-    this.swUpdate.available.subscribe(event => {
-      console.log(
-        '[App] Update available: current version is',
-        event.current,
-        'available version is',
-        event.available
-      );
-      const snackBarRef = this.snackBar.open('Newer version of the app is available', 'Refresh', {
-        duration: MSG_DISPLAY_TIMEOUT
+    this.swUpdate.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe(event => {
+        console.log(
+          '[App] Update available: current version is',
+          event.currentVersion,
+          'available version is',
+          event.latestVersion
+        );
+        const snackBarRef = this.snackBar.open(`Newer version of the app is available: ${event.latestVersion} -> ${event.latestVersion}`, 'update', {
+          duration: MSG_DISPLAY_TIMEOUT * 3
+        });
+        snackBarRef.onAction().subscribe(() => this.activateUpdate());
       });
-      snackBarRef.onAction().subscribe(() => this.activateUpdate());
-    });
 
-    this.swUpdate.activated.subscribe(event => {
-      console.log(
-        '[App] Update activated: old version was',
-        event.previous,
-        'new version is',
-        event.current
+    this.swUpdate.unrecoverable.subscribe(event => {
+      const snackBarRef = this.snackBar.open(
+        'An error occurred that we cannot recover from:\n' +
+        event.reason +
+        '\n\nPlease reload the page.', 'reload'
       );
+      snackBarRef.onAction().subscribe(() => {
+        location.reload();
+      })
     });
   }
 
