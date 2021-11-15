dc011d2a7c6f167389b3feb3c456f4ac9c5e6b1c
metaseed <metaseed@gmail.com>
2021-11-15 09:05:22 -0600

    fix user selection when hammer.js swap

--- workspaces/iam/src/app/hammer.config.ts
+++ workspaces/iam/src/app/hammer.config.ts
@@ -4,9 +4,21 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class MyHammerConfig extends HammerGestureConfig {
+
   overrides = <any>{
     swipe: { direction: hammer.DIRECTION_HORIZONTAL },
     pinch: { enable: false },
-    rotate: { enable: false }
+    rotate: { enable: false },
   };
+
+  constructor(){
+    super();
+    this.enableUserSelection();
+  }
+
+  private enableUserSelection() {
+    // http://hammerjs.github.io/tips/#i-cant-select-my-text-anymore
+    if (Hammer.defaults.cssProps.userSelect)
+      delete Hammer.defaults.cssProps.userSelect;
+  }
 }
