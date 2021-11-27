887eb487497cb498ecd276e38df3c385558a6c83
metaseed <metaseed@gmail.com>
2021-11-26 22:59:11 -0600

    remove delete alert dialog

--- workspaces/iam/src/app/modules/home/doc-list/doc-list-module.ts
+++ workspaces/iam/src/app/modules/home/doc-list/doc-list-module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from 'material';
+import { SpinnerModule } from '../../../../../projects/spinner/src/public_api';
+import { DocDeleteComponent } from './doc-item/doc-delete/doc-delete.component';
 import { DocItemComponent } from './doc-item/doc-item.component';
 
 import { DocListComponent } from './doc-list.component';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, SpinnerModule],
   exports: [DocListComponent],
-  declarations: [DocListComponent, DocItemComponent],
+  declarations: [DocListComponent, DocItemComponent, DocDeleteComponent],
   providers: [],
 })
 export class DocListModule { }
--- workspaces/iam/src/app/modules/home/home.module.ts
+++ workspaces/iam/src/app/modules/home/home.module.ts
@@ -3,8 +3,6 @@ import { HomeComponent } from './home.component';
 import { DocService } from './services/doc.service';
 import { MaterialModule } from '../material/material.module';
 import { SpinnerModule } from '@metaseed/spinner';
-import { DeleteAlertDialogComponent } from './doc-list/deleteAlert-dialog.component';
-import { DocDeleteComponent } from './doc-list/doc-item/doc-delete/doc-delete.component';
 import { SharedModule } from 'shared';
 import { ScrollingModule } from '@angular/cdk-experimental/scrolling';
 import { HomeTopBarComponent } from './home-top-bar/home-top-bar.component';
@@ -15,15 +13,12 @@ import { DocListModule } from './doc-list/doc-list-module';
 @NgModule({
   imports: [SharedModule, ScrollingModule, MaterialModule, SpinnerModule,TagsCloudModule, DocListModule],
   declarations: [
-    DeleteAlertDialogComponent,
     HomeComponent,
     DocListContainerComponent,
-    DocDeleteComponent,
     HomeTopBarComponent,
     DocTreeComponent
   ],
   exports: [HomeComponent],
   providers: [DocService],
-  entryComponents: [DeleteAlertDialogComponent]
 })
 export class HomeModule {}
