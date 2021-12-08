277845a4bb0029ecb3bcdf6a3f20b4f55331b501
metaseed <metaseed@gmail.com>
2021-12-07 23:05:24 -0600

    ok

--- workspaces/iam/src/app/modules/markdown/editor/services/doc-save-coordinate-service.ts
+++ workspaces/iam/src/app/modules/markdown/editor/services/doc-save-coordinate-service.ts
@@ -1,19 +1,20 @@
 import { Inject, Injectable } from '@angular/core';
 import { MarkdownEditorService } from './markdown-editor.service';
 import { BehaviorSubject } from 'rxjs';
-import { combineLatestWith, tap, debounceTime } from 'rxjs/operators';
+import { tap, debounceTime } from 'rxjs/operators';
 import { AUTO_SAVE_TO_DB_AFTER_LAST_EDIT_INTERVAL, backoff, DocFormat, SubscriptionManager } from 'core';
 import { ICodeMirrorEditor } from '../model';
 import { DocumentsEffects, DOCUMENT_EFFECTS_TOKEN } from 'app/modules/shared/store';
 import { OperationStep } from 'packages/rx-store/src/effect';
 import { DocumentStore } from 'app/modules/shared/store/document.store';
+import CodeMirror from 'codemirror';
 
 @Injectable()
 export class DocSaveCoordinateService extends SubscriptionManager {
   isSyncing$ = new BehaviorSubject(false);
   isSaving: boolean;
+  private editor: CodeMirror.Editor;
 
-  private editorLoaded$ = this.editorService.docEditorLoaded$;
   private contentGeneration: number;
 
   constructor(
@@ -23,39 +24,35 @@ export class DocSaveCoordinateService extends SubscriptionManager {
 
   ) {
     super();
-    super
-      .addSub(
-        this.store.currentDocStatus_IsEditorDirty$
-          .pipe(
-            combineLatestWith(this.editorLoaded$),
-            debounceTime(AUTO_SAVE_TO_DB_AFTER_LAST_EDIT_INTERVAL), // e  e e          |
-            tap(([isDirty, editor]) => {
-              if (isDirty)
-                this.documentEffects.saveDocument_.next({ content: editor.getValue(), format: DocFormat.md });
-            }),
-            backoff(8, AUTO_SAVE_TO_DB_AFTER_LAST_EDIT_INTERVAL)
-          ))
-      .addSub(
-        this.editorService.docContentSet$.subscribe((editor: ICodeMirrorEditor) => {
-          this.docLoadedHandler(editor);
-        }))
-      .addSub(
-        this.editorService.docContentModified$
-          .subscribe(([content, editor]) => this.checkDirty(editor)))
-      .addSub(
-        this.documentEffects.saveDocument_.operationStatus$
-          .pipe(
-            combineLatestWith(this.editorLoaded$)
-          )
-          .subscribe(([as, editor]) => {
-            if (as.step === OperationStep.Start) {
-              this.docSavedHandler(editor);
-              this.isSaving = true;
-            } else if (as.isEndStatus()) {
-              this.isSaving = false;
-            }
-          })
-      )
+    super.addSub(
+      this.editorService.docEditorLoaded$.subscribe(editor => this.editor = editor),
+      this.store.currentDocStatus_IsEditorDirty$
+        .pipe(
+          debounceTime(AUTO_SAVE_TO_DB_AFTER_LAST_EDIT_INTERVAL), // e  e e          |
+          tap((isDirty) => {
+            if (isDirty)
+              this.documentEffects.saveDocument_.next({ content: this.editor.getValue(), format: DocFormat.md });
+          }),
+          backoff(8, AUTO_SAVE_TO_DB_AFTER_LAST_EDIT_INTERVAL)
+        ),
+
+      this.editorService.docContentSet$.subscribe((editor: ICodeMirrorEditor) => {
+        this.docLoadedHandler(editor);
+      }),
+
+      this.editorService.docContentModified$
+        .subscribe(([content, editor]) => this.checkDirty(editor)),
+
+      this.documentEffects.saveDocument_.operationStatus$
+        .subscribe((as) => {
+          if (as.step === OperationStep.Start) {
+            this.docSavedHandler(this.editor);
+            this.isSaving = true;
+          } else if (as.isEndStatus()) {
+            this.isSaving = false;
+          }
+        })
+    )
   }
 
   private docLoadedHandler(editor: CodeMirror.Editor) {
@@ -75,7 +72,7 @@ export class DocSaveCoordinateService extends SubscriptionManager {
   private checkDirty(editor: CodeMirror.Editor) {
     let statusInStore = this.store.currentDocStatus$.state;
     if (!statusInStore) {
-      statusInStore = {id: this.store.docStatus.currentId_.state, isEditable: true};
+      statusInStore = { id: this.store.docStatus.currentId_.state, isEditable: true };
       this.store.docStatus.set(statusInStore)
     }
 
